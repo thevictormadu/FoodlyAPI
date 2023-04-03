2b0dbfa890790668b3d044db3eec17b9bdb3fe25
victorpraiz <ivictorpraiz@gmail.com>
2023-04-03 20:07:55 +0100

    local db connection

--- RedMango_API/Data/ApplicationDbContext.cs
+++ RedMango_API/Data/ApplicationDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace RedMango_API.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedMango_API.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
