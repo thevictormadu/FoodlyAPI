a458c4659195b78b04a3104b72cca8e6390556c0
victorpraiz <ivictorpraiz@gmail.com>
2023-04-04 11:07:30 +0100

    get, delete and upload blob service to azure

--- RedMango_API/Controllers/MenuItemController.cs
+++ RedMango_API/Controllers/MenuItemController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RedMango_API.Data;
 using RedMango_API.Models;
+using System.Net;
 
 namespace RedMango_API.Controllers
 {
@@ -21,7 +22,28 @@ namespace RedMango_API.Controllers
         public async Task<IActionResult> GetMenuItems()
         {
             _response.Result = _db.MenuItems;
-            _response.StatusCode = System.Net.HttpStatusCode.OK;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetMenuItems(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode= HttpStatusCode.BadRequest;
+                return BadRequest(_response);
+            }
+
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+
+            if (menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+            _response.Result = menuItem;
+            _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
         }
     }
