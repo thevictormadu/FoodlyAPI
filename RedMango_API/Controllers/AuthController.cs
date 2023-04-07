67664103f48edf5bae390cc5d389b28531bcd6bc
victorpraiz <ivictorpraiz@gmail.com>
2023-04-07 23:51:35 +0100

    user registration and roles api

---
+++ RedMango_API/Controllers/AuthController.cs
@@ -0,0 +1,104 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using RedMango_API.Models.Dto;
+using RedMango_API.Services;
+using RedMango_API.Utility;
+using System.Net;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/[auth]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        private string secretKey;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+       
+        public AuthController(ApplicationDbContext db, IConfiguration configuration, 
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _response = new ApiResponse();
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _roleManager = roleManager;
+            _userManager = userManager;
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto model)
+        {
+            try
+            {
+                ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
+
+                if (userFromDb != null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages.Add("Username Already Exist");
+                    return BadRequest(_response);
+                }
+
+
+
+                ApplicationUser newUser = new()
+                {
+                    UserName = model.UserName,
+                    Email = model.UserName,
+                    NormalizedEmail = model.UserName.ToUpper(),
+                    Name = model.Name,
+                };
+
+                var result = await _userManager.CreateAsync(newUser, model.Password);
+
+                if (result.Succeeded)
+                {
+                    if (_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                    {
+                        //create roles in database
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin));
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer));
+                    }
+
+                    if (model.Role.ToLower() == SD.Role_Admin)
+                    {
+                        _userManager.AddToRoleAsync(newUser, SD.Role_Admin);
+                    }
+                    else
+                    {
+                        _userManager.AddToRoleAsync(newUser, SD.Role_Customer);
+                    }
+
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+
+                }
+
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.ErrorMessages.Add("Error While Registering");
+                return BadRequest(_response);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode= HttpStatusCode.BadRequest;
+                _response.ErrorMessages.Add(ex.Message);
+                return BadRequest(_response);
+
+            }
+
+
+        }
+    }
+}
