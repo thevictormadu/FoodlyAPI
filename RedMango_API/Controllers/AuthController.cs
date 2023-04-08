f89aac40f54504366f324ea2028c97cd8f81d81a
victorpraiz <ivictorpraiz@gmail.com>
2023-04-08 02:56:17 +0100

    JWT Token generator

--- RedMango_API/Controllers/AuthController.cs
+++ RedMango_API/Controllers/AuthController.cs
@@ -2,16 +2,20 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RedMango_API.Data;
 using RedMango_API.Models;
 using RedMango_API.Models.Dto;
 using RedMango_API.Services;
 using RedMango_API.Utility;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 
 namespace RedMango_API.Controllers
 {
-    [Route("api/Auth")]
+    [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
     {
@@ -32,7 +36,73 @@ namespace RedMango_API.Controllers
             
         }
 
-        [HttpPost]
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponseDto>> Login([FromBody] LoginRequestDto model)
+        {
+            ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.UserName == model.UserName);
+            
+
+            var isValid = await _userManager.CheckPasswordAsync(userFromDb, model.Password);
+
+            if (!isValid)
+            {
+                _response.Result = new LoginResponseDto();
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.ErrorMessages.Add("Username or Password Is Incorrect");
+                _response.IsSuccess= false;
+                return BadRequest(_response);
+            }
+
+            //generate JWT token
+            var roles = await _userManager.GetRolesAsync(userFromDb);
+            var symmetricKey = Encoding.ASCII.GetBytes(secretKey);
+            JwtSecurityTokenHandler tokenHandler= new JwtSecurityTokenHandler();
+
+            SecurityTokenDescriptor securityTokenDescriptor = new ()
+            { 
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Fullname", userFromDb.Name),
+                    new Claim("Id", userFromDb.Id.ToString()),
+                    new Claim(ClaimTypes.Email, userFromDb.UserName.ToString()),
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                    
+                }),
+                Expires= DateTime.UtcNow.AddMinutes(10),
+
+                SigningCredentials= new SigningCredentials(new SymmetricSecurityKey(symmetricKey),
+                SecurityAlgorithms.HmacSha256Signature),
+             
+            };
+
+            var stoken = tokenHandler.CreateToken(securityTokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+
+
+            LoginResponseDto loginResponseDto = new()
+            {
+                Email = userFromDb.UserName,
+                Token = token
+            };
+
+            if (loginResponseDto.Email == null || string.IsNullOrEmpty(loginResponseDto.Token))
+            {
+                _response.Result = new LoginResponseDto();
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.ErrorMessages.Add("Username or Password Is Incorrect");
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+
+            }
+
+            _response.Result = loginResponseDto;
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            return Ok(_response);
+        }
+
+        [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDto model)
         {
             
