ee45546868b566830df91a3f0b1bdd23cdece163
victorpraiz <ivictorpraiz@gmail.com>
2023-04-13 18:57:56 +0100

    autorize annotation

---
+++ RedMango_API/Controllers/AuthTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/AuthTest")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetSomething()
+        {
+            return "you are authenticated";
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<string>> GetSomething(int someIntValue)
+        {
+            return "you are authorized with role of admin";
+        }
+    }
+}
--- RedMango_API/Program.cs
+++ RedMango_API/Program.cs
@@ -40,7 +40,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
