e0b73bc04a4ad14fed367689ba35f2ee9562b032
victorpraiz <ivictorpraiz@gmail.com>
2023-04-14 23:00:05 +0100

    Shopping Cart Api

--- RedMango_API/Migrations/20230413222921_added shopping cart.Designer.cs
+++ RedMango_API/Migrations/20230413222921_added shopping cart.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RedMango_API.Data;
 
@@ -11,9 +12,11 @@ using RedMango_API.Data;
 namespace RedMango_API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230413222921_added shopping cart")]
+    partial class addedshoppingcart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -391,6 +394,12 @@ namespace RedMango_API.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("ClientSecret")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("StripePaymentIntentId")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("UserId")
                         .HasColumnType("nvarchar(max)");
---
+++ RedMango_API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RedMango_API.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
+
+        [NotMapped]
+        public double CardTotal { get; set; }
+        [NotMapped]
+        public string StripePaymentIntentId { get; set; }
+        [NotMapped]
+        public string ClientSecret { get; set; }
+    }
+}
